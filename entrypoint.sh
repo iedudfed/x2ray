2dd4d8935a139d5edd1f2e7679978f392e60ab04
wangyi2005 <wangyi2005@users.noreply.github.com>
2018-03-02 23:09:50 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@ cd /v2raybin
 curl -L -o v2ray.zip https://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
 unzip v2ray.zip 
 cd /v2raybin/v2ray-v$VER-linux-64
+chmod +x /v2raybin/v2ray
+chmod +x /v2raybin/v2ctl
 #mv /v2raybin/v2ray-v$VER-linux-64/v2ray /v2raybin/ \
 #mv /v2raybin/v2ray-v$VER-linux-64/v2ctl /v2raybin/ \
 #mv /v2raybin/v2ray-v$VER-linux-64/geoip.dat /v2raybin/ \
