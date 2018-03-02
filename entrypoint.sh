80cce4814974e30bffc20cb44059c2a2fa9c034d
wangyi2005 <wangyi2005@users.noreply.github.com>
2018-03-02 23:05:44 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 cd /v2raybin
-curl -L -H "Cache-Control: no-cache" -o v2ray.zip https://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
+curl -L -o v2ray.zip https://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
 unzip v2ray.zip 
 cd /v2raybin/v2ray-v$VER-linux-64
 #mv /v2raybin/v2ray-v$VER-linux-64/v2ray /v2raybin/ \
