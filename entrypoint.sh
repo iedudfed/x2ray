e4883382d1e5812697420f1e5f1df7dd11994a0f
wangyi2005 <wangyi2005@users.noreply.github.com>
2018-03-03 10:39:53 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 cd /v2raybin
-curl -L -o v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
+#curl -L -o v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
+wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
 unzip v2ray.zip 
 cd /v2raybin/v2ray-v$VER-linux-64
 chmod +x v2ray
